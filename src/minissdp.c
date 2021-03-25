70da4529e3eb742821a1c777a3a0ca1848bbda23
kgrze <grzeszczak.krzysztof@gmail.com>
2021-03-25 12:02:29 +0100

    fix error with UDP multicast socket

--- src/minissdp.c
+++ src/minissdp.c
@@ -56,6 +56,38 @@
 #define SSDP_PORT (1900)
 #define SSDP_MCAST_ADDR ("239.255.255.250")
 
+static int
+AddMulticastMembership(int s, struct lan_addr_s *iface)
+{
+	int ret;
+#ifdef HAVE_STRUCT_IP_MREQN
+	struct ip_mreqn imr;	/* Ip multicast membership */
+	/* setting up imr structure */
+	memset(&imr, '\0', sizeof(imr));
+	imr.imr_multiaddr.s_addr = inet_addr(SSDP_MCAST_ADDR);
+	imr.imr_ifindex = iface->ifindex;
+#else
+	struct ip_mreq imr;	/* Ip multicast membership */
+	/* setting up imr structure */
+	memset(&imr, '\0', sizeof(imr));
+	imr.imr_multiaddr.s_addr = inet_addr(SSDP_MCAST_ADDR);
+	imr.imr_interface.s_addr = iface->addr.s_addr;
+#endif
+	/* Setting the socket options will guarantee, tha we will only receive
+	 * multicast traffic on a specific Interface.
+	 * In addition the kernel is instructed to send an igmp message (choose
+	 * mcast group) on the specific interface/subnet. */
+	ret = setsockopt(s, IPPROTO_IP, IP_ADD_MEMBERSHIP, (void *)&imr, sizeof(imr));
+	if (ret < 0 && errno != EADDRINUSE)
+	{
+		DPRINTF(E_ERROR, L_SSDP, "setsockopt(udp, IP_ADD_MEMBERSHIP): %s\n",
+			strerror(errno));
+		return -1;
+	}
+
+	return 0;
+}
+
 /* Open and configure the socket listening for 
  * SSDP udp packets sent on 239.255.255.250 port 1900 */
 int
@@ -110,12 +142,18 @@ int
 OpenAndConfSSDPNotifySocket(struct lan_addr_s *iface)
 {
 	int s;
-	
+		
 	s = socket(PF_INET, SOCK_DGRAM, 0);
 	if (s < 0)
 	{
 		DPRINTF(E_ERROR, L_SSDP, "socket(udp_notify): %s\n", strerror(errno));
 		return -1;
+	}
+
+	if (AddMulticastMembership(sssdp, iface) < 0)
+	{
+		DPRINTF(E_WARN, L_SSDP, "Failed to add multicast membership for address %s\n", 
+			iface->str);
 	}
 
 	return s;
